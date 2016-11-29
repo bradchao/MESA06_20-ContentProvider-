46ba3957fe62401f4cdd9f506593639ca2e9f141
Brad Chao <brad@brad.tw>
2016-11-29 09:43:40 +0800

    v4

--- app/src/main/java/com/example/user/brad20/MainActivity.java
+++ app/src/main/java/com/example/user/brad20/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.user.brad20;
 import android.Manifest;
 import android.accounts.Account;
 import android.accounts.AccountManager;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.provider.ContactsContract;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -23,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.GET_ACCOUNTS)
+                Manifest.permission.READ_CONTACTS)
                 != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this,
                     new String[]{Manifest.permission.READ_PHONE_STATE,
-                            Manifest.permission.GET_ACCOUNTS},
+                            Manifest.permission.GET_ACCOUNTS,
+                        Manifest.permission.READ_CONTACTS},
                     123);
         }else{
             init();
@@ -38,7 +42,6 @@ public class MainActivity extends AppCompatActivity {
         tmgr = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
         Log.v("brad", "機馬" +tmgr.getDeviceId());
         Log.v("brad", "ｓｉｍ" +tmgr.getSubscriberId());
-
         tmgr.listen(new MyPhoneStateListener(), PhoneStateListener.LISTEN_CALL_STATE);
 
         amgr = (AccountManager)getSystemService(ACCOUNT_SERVICE);
@@ -47,8 +50,36 @@ public class MainActivity extends AppCompatActivity {
             Log.v("brad", a.name + ":" +a.type );
         }
 
+        getContact();
+
 
     }
+
+    private void getContact(){
+        Log.v("brad", "OK");
+        ContentResolver contentResolver = getContentResolver();
+        String[] projection = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER
+        };
+        Cursor cursor = contentResolver.query(
+                ContactsContract.CommonDataKinds.Phone.CONTENT_URI,
+                projection, null, null,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+
+        Log.v("brad", "count: " + cursor.getCount());
+
+        while ( cursor.moveToNext()){
+            String name = cursor.getString(0);
+            String tel = cursor.getString(1);
+            Log.v("brad", name + ":" + tel);
+        }
+
+
+
+
+    }
+
+
     private class MyPhoneStateListener extends PhoneStateListener {
         @Override
         public void onCallStateChanged(int state, String incomingNumber) {
